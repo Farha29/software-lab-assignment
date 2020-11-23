ac2dcfda0dd13812b4e01722cd58ec5bfe51536e
Farha29 <67853823+Farha29@users.noreply.github.com>
2020-11-23 20:53:48 +0600

    Update one.c

--- one.c
+++ one.c
@@ -2,8 +2,16 @@
 
 int main()
 {
-   printf("1.\n");
-   printf("2\n");
+   int a,b,sum;
+
+   printf("Please Enter the 1st Number: ");
+   scanf("%d",&a);
+   printf("Please Enter the 2nd Number: ");
+   scanf("%d",&b);
+
+   sum=a+b;
+
+   printf("\nThe sum of %d and %d is: %d\n",a,b,sum);
 
    return 0;
 }
